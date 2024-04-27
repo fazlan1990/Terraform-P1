e3a3420f4e808d2070d970a8411c340c7f6defda
fazlan1990 <ahmad@gmail.com>
2024-04-27 06:47:01 +0530

    m7

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
     resource_group_name = "Terraform"
     storage_account_name = "terraformfas2"
     container_name       = "tfstatefile"
-    key                  = "de.terraform.tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
